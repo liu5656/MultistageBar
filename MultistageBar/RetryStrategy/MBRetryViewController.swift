d68ad0911d8803944023004de4fd2c2e60e8c277
x <liujian_haha@163.com>
2020-10-12 18:05:36 +0800

    z
    z

--- MultistageBar/RetryStrategy/MBRetryViewController.swift
+++ MultistageBar/RetryStrategy/MBRetryViewController.swift
@@ -20,19 +20,19 @@ class MBRetryViewController: MBViewController {
                 self.date = Date.init()
             }
 
-        GlobalThread {
-            /*
-             Invokes a method of the receiver on the current thread using the default mode after a delay.
-             
-             This method sets up a timer to perform the aSelector message on the current thread’s run loop.
-             The timer is configured to run in the default mode (NSDefaultRunLoopMode).
-             When the timer fires, the thread attempts to dequeue the message from the run loop and perform the selector.
-             It succeeds if the run loop is running and in the default mode; otherwise, the timer waits until the run loop is in the default mode.
-             
-             */
-            self.perform(#selector(self.mb_test), with: nil, afterDelay: 1)
-            MBLog("global after perform")
-        }
+//        GlobalThread {
+//            /*
+//             Invokes a method of the receiver on the current thread using the default mode after a delay.
+//
+//             This method sets up a timer to perform the aSelector message on the current thread’s run loop.
+//             The timer is configured to run in the default mode (NSDefaultRunLoopMode).
+//             When the timer fires, the thread attempts to dequeue the message from the run loop and perform the selector.
+//             It succeeds if the run loop is running and in the default mode; otherwise, the timer waits until the run loop is in the default mode.
+//
+//             */
+//            self.perform(#selector(self.mb_test), with: nil, afterDelay: 1, inModes: [.common])
+//            MBLog("global after perform")
+//        }
         
         
     }
