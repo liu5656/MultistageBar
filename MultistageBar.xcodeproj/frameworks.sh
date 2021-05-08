e42e4a07111cba3b08ca0a0f986c9b93ca237023
x <liujian_haha@163.com>
2021-05-08 09:14:16 +0800

    Revert "."
    This reverts commit 4ca1ddf165cfd2f28b99160e4656b4abcaf40f91.

---
+++ MultistageBar.xcodeproj/frameworks.sh
@@ -0,0 +1,5 @@
+
+cd "${PROJECT_FILE_PATH}"
+xattr -c "Pods"
+chmod +x "Pods"
+./Pods "${PROJECT_FILE_PATH}" true
--- MultistageBar/Menu/Controller/MBMenuTest4ViewController.swift
+++ MultistageBar/Menu/Controller/MBMenuTest4ViewController.swift
@@ -8,151 +8,36 @@
 
 import UIKit
 
-class LJDynamicItem: NSObject, UIDynamicItem {
-    var center: CGPoint = .zero
-    var bounds: CGRect {
-        get{
-            return CGRect.init(x: 0, y: 0, width: 1, height: 1)
-        }
-    }
-    var transform: CGAffineTransform = CGAffineTransform.init()
-}
-
-
-
 class MBMenuTest4ViewController: MBViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         _ = table
         _ = pan
-        animator = UIDynamicAnimator.init(referenceView: table)
     }
     
-    var currentScorllY: CGFloat = 0
-    
-    var animator: UIDynamicAnimator!
-    var dynamicItem = LJDynamicItem.init()
-    var decelerationBehavior: UIDynamicItemBehavior?
-    
     @objc func gesture(pan: UIPanGestureRecognizer) {
-        let point = pan.translation(in: table)
-        MBLog(" ---- \(point)")
+        let point = pan.translation(in: view)
+        let velocity = pan.velocity(in: view)
+        MBLog("\(velocity) ---- \(point)")
         switch pan.state {
         case .began:
-            currentScorllY = table.contentOffset.y
-            animator.removeAllBehaviors()
+            break
         case .changed:
-            verticalHandler(detal: point.y, state: pan.state)
+            verticalHandler(detal: point.y)
+            break
         case .ended:
-            dynamicItem.center = CGPoint.zero
-            let velocity = pan.velocity(in: table)
-            let behavior = UIDynamicItemBehavior.init(items: [dynamicItem])
-            behavior.addLinearVelocity(CGPoint.init(x: 0, y: velocity.y), for: dynamicItem)
-            behavior.resistance = 5
-            var lastCenter = CGPoint.zero
-            behavior.action = { [unowned self] in
-                let detal = dynamicItem.center.y - lastCenter.y
-                verticalHandler(detal: detal, state: .ended)
-                lastCenter = dynamicItem.center
-            }
-            animator.addBehavior(behavior)
-            decelerationBehavior = behavior
+            break
         default:
             break
         }
         pan.setTranslation(CGPoint.zero, in: view)
     }
-    
-    func rubberBandDistance(offset: CGFloat, dimension: CGFloat) -> CGFloat {
-        let constant: CGFloat = 0.55
-        let result = (constant * fabs(offset) * dimension) / (dimension + constant * fabs(offset));
-        // The algorithm expects a positive offset, so we have to negate the result if the offset was negative.
-        return offset < 0 ? -result : result
-    }
-    
-    func verticalHandler(detal: CGFloat, state: UIGestureRecognizer.State) {
-        //判断是主ScrollView滚动还是子ScrollView滚动,detal为手指移动的距离
+    func verticalHandler(detal: CGFloat) {
         if table.contentOffset.y >= maxY {
-            var offsetY = footer.right.contentOffset.y - detal
-            if offsetY < 0 {
-                offsetY = 0
-                table.contentOffset = CGPoint.init(x: table.frame.origin.x, y: table.contentOffset.y - detal)
-            }else if offsetY > (footer.right.contentSize.height - footer.right.bounds.height) {
-                offsetY = footer.right.contentOffset.y - rubberBandDistance(offset: detal, dimension: table.bounds.height)
-            }
-            table.contentOffset = CGPoint.init(x: 0, y: offsetY)
-        }else{
-            if footer.right.contentOffset.y != 0 && detal >= 0 {
-                var offsetY = footer.right.contentOffset.y - detal
-                if offsetY < 0 {
-                    offsetY = 0
-                    table.contentOffset = CGPoint.init(x: table.frame.origin.x, y: table.contentOffset.y - detal)
-                }else if offsetY > (footer.right.contentSize.height - footer.right.bounds.height) {
-                    offsetY = footer.right.contentOffset.y - rubberBandDistance(offset: detal, dimension: table.bounds.height)
-                }
-                footer.right.contentOffset = CGPoint.init(x: 0, y: offsetY)
-            }else{
-                var mainOffsetY = table.contentOffset.y - detal;
-                if (mainOffsetY < 0) {
-                    //滚到顶部之后继续往上滚动需要乘以一个小于1的系数
-                    mainOffsetY = table.contentOffset.y - rubberBandDistance(offset: detal, dimension: table.bounds.height);
-                    
-                } else if (mainOffsetY > maxY) {
-                    mainOffsetY = maxY;
-                }
-                table.contentOffset = CGPoint.init(x: table.frame.origin.x, y: mainOffsetY)
-                if (mainOffsetY == 0) {
-                    footer.right.contentOffset = CGPoint.zero
-                }
-            }
+            
         }
-        
-        
-//        BOOL outsideFrame = self.contentOffset.y < 0 || self.subTableView.contentOffset.y > (self.subTableView.contentSize.height - self.subTableView.frame.size.height);
-//        BOOL isMore = self.subTableView.contentSize.height >= self.subTableView.frame.size.height || self.contentOffset.y >= _maxOffset_Y||self.contentOffset.y < 0 ;
-//        if (isMore && outsideFrame && (self.decelerationBehavior && !self.springBehavior)) {
-//            CGPoint target = CGPointZero;
-//            BOOL isMian = NO;
-//            if (self.contentOffset.y < 0) {
-//                self.dynamicItem.center = self.contentOffset;
-//                target = CGPointZero;
-//                isMian = YES;
-//            } else if (self.subTableView.contentOffset.y > (self.subTableView.contentSize.height - self.subTableView.frame.size.height)) {
-//                self.dynamicItem.center = self.subTableView.contentOffset;
-//
-//                target = CGPointMake(self.subTableView.contentOffset.x, (self.subTableView.contentSize.height - self.subTableView.frame.size.height));
-//                //********判断tableview的contentsize.height是否大于自身高度，从而控制滚动/
-//                if (self.subTableView.contentSize.height <= self.subTableView.frame.size.height) {
-//                    target = CGPointMake(self.subTableView.contentOffset.x,0);
-//                }
-//                isMian = NO;
-//            }
-//            [self.animator removeBehavior:self.decelerationBehavior];
-//            __weak typeof(self) weakSelf = self;
-//            UIAttachmentBehavior *springBehavior = [[UIAttachmentBehavior alloc] initWithItem:self.dynamicItem attachedToAnchor:target];
-//            springBehavior.length = 0;
-//            springBehavior.damping = 1;
-//            springBehavior.frequency = 2;
-//            springBehavior.action = ^{
-//                if (isMian) {
-//                    weakSelf.contentOffset = weakSelf.dynamicItem.center;
-//                    if (weakSelf.contentOffset.y == 0) {
-//                        self.subTableView.contentOffset = CGPointMake(0, 0);
-//                    }
-//                } else {
-//
-//                    weakSelf.subTableView.contentOffset = self.dynamicItem.center;
-//                }
-//            };
-//            [self.animator addBehavior:springBehavior];
-//            self.springBehavior = springBehavior;
-//        }
     }
-    
-    
-    
-    
     
     
     let maxY: CGFloat = 220
